17707e696e2bfd90903a74e0fda0b53ee8a92562
bang9 <gusrn1423@naver.com>
2023-06-10 23:28:10 +0900

    chore: update

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,14 +2,20 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import terser from '@rollup/plugin-terser';
+// import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-    manualChunks: () => 'dist/index.js'
+    dir: 'dist',
+    format: 'cjs'
   },
-  plugins: [json(), commonjs(), resolve(), typescript(), terser({ compress: true })]
+  plugins: [
+    json(),
+    commonjs(),
+    resolve(),
+    typescript({ outDir: 'dist' })
+    //terser({ compress: true })
+    // ____
+  ]
 };
